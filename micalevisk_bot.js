39941a23ebd22900128c176e0ddcc3cda5e4c4a2
micalevisk <mllc@icomp.ufam.edu.br>
2017-02-11 13:32:37 -0400

    fix log msg (when received any message)

--- micalevisk_bot.js
+++ micalevisk_bot.js
@@ -11,13 +11,24 @@ var bot = function () {
 	/// Create a bot that uses 'polling' to fetch new updates
 	var bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
+	// Listen for any kind of message. There are different kinds of
+	// messages.
+	bot.on('message', function (msg) {
+		var log = {
+			  chatId: msg.chat.id
+			, from: msg.chat.username
+			, text: msg.text
+		};
+
+		console.log("[RECEBEU MSG]", log);
+	});
+
 	/// Matches "/echo <text>"
 	bot.onText(/\/echo (.+)/i, function (msg, match) {
 		/// 'msg' is the received Message from Telegram 'match' is the result of executing the regexp above on the text content of the message
 		var chatId = msg.chat.id;
 		var resp = match[1].toUpperCase() + ", " + msg.chat.username;
-
-		console.log("[RECEBEU]", msg);
+		
 		bot.sendMessage(chatId, resp);
 	});
 
